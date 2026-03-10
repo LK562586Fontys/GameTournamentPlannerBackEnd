a4a0795af3d220bd737068a400ec9414834242c1
LK <lucaskaethoven6@gmail.com>
2026-03-10 10:00:16 +0100

    Added the project for the backend
    March 10th 2026

---
+++ gametournamentplanner/src/test/java/com/example/gametournamentplanner/GametournamentplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gametournamentplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametournamentplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
