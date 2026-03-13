eb086fa02bc888e2c5f11bba10b4ef628a8ee1f4
LK <lucaskaethoven6@gmail.com>
2026-03-13 13:27:51 +0100

    created an application-test.properties file for applicationtests
    march 13th 2026

--- gametournamentplanner/src/test/java/com/example/gametournamentplanner/GametournamentplannerApplicationTests.java
+++ gametournamentplanner/src/test/java/com/example/gametournamentplanner/GametournamentplannerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.gametournamentplanner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GametournamentplannerApplicationTests {
 
     @Test
