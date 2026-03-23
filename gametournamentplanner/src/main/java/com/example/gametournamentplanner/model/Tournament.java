7e6408e1ac112cd8127c9e5a9f9081e07fcfb7d5
LK <lucaskaethoven6@gmail.com>
2026-03-23 15:38:38 +0100

    added getters to each value
    March 23rd 2026

--- gametournamentplanner/src/main/java/com/example/gametournamentplanner/model/Tournament.java
+++ gametournamentplanner/src/main/java/com/example/gametournamentplanner/model/Tournament.java
@@ -11,15 +11,34 @@ public class Tournament {
     @GeneratedValue
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
     private String name;
     public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
+
 
     private Long gameId;
     public void setGameId(Long gameId) { this.gameId = gameId; }
+    public Long getGameId() {
+        return gameId;
+    }
 
     private String rules;
     public void setRules(String rules) { this.rules = rules; }
 
+    public String getRules() {
+        return rules;
+    }
+
     private Long maxParticipants;
     public void setMaxParticipants(Long maxParticipants) { this.maxParticipants = maxParticipants; }
+
+    public Long getMaxParticipants() {
+        return maxParticipants;
+    }
 }
