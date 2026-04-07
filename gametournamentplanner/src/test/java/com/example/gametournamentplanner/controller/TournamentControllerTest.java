0b048063b46ae551e9b82d2bc0e0c9b21b273e56
LK <lucaskaethoven6@gmail.com>
2026-04-07 12:21:32 +0200

    fixed the tests to make them work by changing springboot version
    april 7th 2026

--- gametournamentplanner/src/test/java/com/example/gametournamentplanner/controller/TournamentControllerTest.java
+++ gametournamentplanner/src/test/java/com/example/gametournamentplanner/controller/TournamentControllerTest.java
@@ -2,6 +2,7 @@ package com.example.gametournamentplanner.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class TournamentControllerTest {
 
--- gametournamentplanner/src/test/java/com/example/gametournamentplanner/repository/TournamentRepositoryTest.java
+++ gametournamentplanner/src/test/java/com/example/gametournamentplanner/repository/TournamentRepositoryTest.java
@@ -3,12 +3,14 @@ package com.example.gametournamentplanner.repository;
 import com.example.gametournamentplanner.model.Tournament;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DataJpaTest
 @ActiveProfiles("test")
 class TournamentRepositoryTest {
 
