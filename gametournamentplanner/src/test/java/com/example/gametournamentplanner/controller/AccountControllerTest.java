3e83777e2bd9b12c255ab4d50a1b66b43e04e2ba
LK <562586@student.fontys.nl>
2026-06-06 09:01:37 +0200

    added a get test & fixed the exception
    june 6th 2026

--- gametournamentplanner/src/test/java/com/example/gametournamentplanner/controller/AccountControllerTest.java
+++ gametournamentplanner/src/test/java/com/example/gametournamentplanner/controller/AccountControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.gametournamentplanner.controller;
 
 
+import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +10,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -43,26 +46,17 @@ class AccountControllerTest {
                 .andExpect(status().isOk());
 
     }
-    /*
+
     @WithMockUser
     @Test
-    void ShouldntCreateAccount_RejectDuplicateEmail() throws Exception {
+    void ShouldntCreateAccount_AccountAlreadyExists() throws Exception {
 
         String json =
                 """
                 {
                     "name": "HSchrader",
-                    "emailAddress": "test@hotmail.com",
+                    "emailAddress": "hschrader@hotmail.com",
                     "password": "HSchrader2$"
-                }
-                """;
-
-        String json2 =
-                """
-                {
-                    "name": "WWhite",
-                    "emailAddress": "test@hotmail.com",
-                    "password": "WWhite2$"
                 }
                 """;
 
@@ -71,14 +65,14 @@ class AccountControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json));
 
-        mockMvc.perform(post("/api/accounts")
+        assertThrows(ServletException.class, () ->
+                mockMvc.perform(post("/api/accounts")
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json2))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string("Duplicate email address"));
+                        .content(json))
+        );
     }
-    */
+
     @Test
     @WithMockUser
     void ShouldLogin() throws Exception {
@@ -105,5 +99,24 @@ class AccountControllerTest {
                 .andExpect(jsonPath("$.token").exists())
                 .andExpect(jsonPath("$.emailAddress")
                         .value("m.ehrmantrout@gmail.com"));
+    }
+    @Test
+    @WithMockUser
+    void ShouldGetAllUsers() throws Exception {
+
+        String json = """
+                {
+                    "name":"GFring",
+                    "emailAddress": "g.fring@gmail.com"
+                    "password": "password123!"
+                }
+                """;
+        mockMvc.perform(post("/api/accounts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json));
+
+        mockMvc.perform(get("/api/accounts"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
     }
 }
